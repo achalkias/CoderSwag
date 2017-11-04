b8b2290265472451c2807781cf1b335db88ec50b
achalkias <achalkias@hotmail.com>
2017-11-05 01:09:59 +0200

    Architecture and data services

--- app/src/main/java/com/achalkias/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/achalkias/coderswag/controller/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.achalkias.coderswag
+package com.achalkias.coderswag.controller
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.achalkias.coderswag.R
 
 class MainActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/com/achalkias/coderswag/model/Category.kt
@@ -0,0 +1,7 @@
+package com.achalkias.coderswag.model
+
+/**
+ * Created by tolis on 11/5/2017.
+ */
+class Category(val title: String, val image: String) {
+}
