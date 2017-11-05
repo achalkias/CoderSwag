a1bc4a06e065836c716a758f58fccc836afffa3e
achalkias <achalkias@hotmail.com>
2017-11-05 02:51:49 +0200

    Demo finished

--- app/src/main/java/com/achalkias/coderswag/controller/ProductsActivity.kt
+++ app/src/main/java/com/achalkias/coderswag/controller/ProductsActivity.kt
@@ -1,17 +1,44 @@
 package com.achalkias.coderswag.controller
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
+import com.achalkias.coderswag.Adapters.ProductsAdapter
 import com.achalkias.coderswag.R
+import com.achalkias.coderswag.services.DataService
 import com.achalkias.coderswag.utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 3
+        }
+
+        val screeenSize = resources.configuration.screenWidthDp
+
+        if (screeenSize > 720) {
+            spanCount = 3
+        }
+
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+        val layoutManager = GridLayoutManager(this, spanCount)
+        productRecyclerView.layoutManager = layoutManager
+        productRecyclerView.adapter = adapter
+
+        textViewTitle.text = categoryType
 
 
     }
